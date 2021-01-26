a1079bb7a7d4437e601c4497aa5ea6495a272b67
Evan Glaser <glaser.evan@gmail.com>
2021-01-25 19:57:02 -0700

    server built

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3131;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`we are listening on PORT: ${PORT}`);
+});
