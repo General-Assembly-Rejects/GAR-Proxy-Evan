e2f9201fe6f38f528f6e7e4748b42d6c01932827
Evan Glaser <glaser.evan@gmail.com>
2021-01-27 11:40:19 -0700

    updated to port 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3131;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
